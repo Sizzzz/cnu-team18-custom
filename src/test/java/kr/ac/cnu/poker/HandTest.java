49e27dea92eb340da8eb2732f288e4ffb29c4211
BonCheul <bonchecul92nd@naver.com>
2017-06-15 16:24:18 +0900

    create testing dir and create Hand class tester class

---
+++ src/test/java/kr/ac/cnu/poker/HandTest.java
@@ -0,0 +1,17 @@
+package kr.ac.cnu.poker;
+
+import org.junit.Test;
+
+/**
+ * Created by USER on 2017-06-15.
+ */
+public class HandTest {
+    @Test
+    public void 파이브카드_핸드_카드숫자는_5() {
+
+    }
+    @Test
+    public void 세븐카드_핸드_카드숫자는_7() {
+
+    }
+}
