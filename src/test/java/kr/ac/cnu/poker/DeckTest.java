9913ddcc4a0e66792311426dcf7bf1f137711737
Lee DongSeop <dajababa09@gmail.com>
2017-06-15 17:08:14 +0900

    Create DeckTest

--- src/test/java/kr/ac/cnu/poker/DeckTest.java
+++ src/test/java/kr/ac/cnu/poker/DeckTest.java
@@ -1,7 +1,20 @@
 package kr.ac.cnu.poker;
 
+import kr.ac.cnu.service.poker.Deck;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by dajab on 2017-06-15.
  */
 public class DeckTest {
+
+    @Test
+    public void Deck2개의_총카드수는_104(){
+        Deck deck = new Deck(2);
+        int totalCard = deck.getTotalCard();
+        assertThat(totalCard, is(104));
+    }
 }
