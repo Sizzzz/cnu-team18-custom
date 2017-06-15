020b90de19442567cd4ca7689ca758ea44796756
swxss124 <swxss124@naver.com>
2017-06-15 17:22:33 +0900

    Add evaluator : zokbo list

--- src/main/java/kr/ac/cnu/service/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/service/poker/Evaluator.java
@@ -1,7 +1,68 @@
 package kr.ac.cnu.service.poker;
 
+import java.util.List;
+
 /**
  * Created by sizz on 2017-06-15.
  */
 public class Evaluator {
+    public int evaluate(List<Card> cardList) {
+
+        if(isROYAL_STRAIGHT_FLUSH(cardList)) {
+            return Zokbo.ROYAL_FLUSH.getZokboRank();
+        }
+        else if(isSTRAIGHT_FLUSH(cardList)) {
+            return Zokbo.STRAIGHT_FLUSH.getZokboRank();
+        }
+        else if(isFOUR_OF_A_KIND(cardList)) {
+            return Zokbo.FOUR_OF_A_KIND.getZokboRank();
+        }
+        else if(isFULL_HOUSE(cardList)) {
+            return Zokbo.FULL_HOUSE.getZokboRank();
+        }
+        else if(isFLUSH(cardList)) {
+            return Zokbo.FLUSH.getZokboRank();
+        }
+        else if(isSTRAIGHT(cardList)) {
+            return Zokbo.STRAIGHT.getZokboRank();
+        }
+        else if(isTHREE_OF_A_KIND(cardList)) {
+            return Zokbo.THREE_OF_A_KIND.getZokboRank();
+        }
+        else if(isTWO_PAIR(cardList)) {
+            return Zokbo.TWO_PAIR.getZokboRank();
+        }
+        else if(isPAIR(cardList)) {
+            return Zokbo.PAIR.getZokboRank();
+        }
+        else
+            return Zokbo.HIGH_CARD.getZokboRank();
+    }
+
+    private boolean isROYAL_STRAIGHT_FLUSH(List<Card> cardList) {
+    }
+
+    private boolean isSTRAIGHT_FLUSH(List<Card> cardList) {
+    }
+
+    private boolean isFOUR_OF_A_KIND(List<Card> cardList) {
+    }
+
+    private boolean isFULL_HOUSE(List<Card> cardList) {
+    }
+
+    private boolean isFLUSH(List<Card> cardList) {
+    }
+
+    private boolean isSTRAIGHT(List<Card> cardList) {
+    }
+
+    private boolean isTHREE_OF_A_KIND(List<Card> cardList) {
+    }
+
+    private boolean isTWO_PAIR(List<Card> cardList) {
+    }
+
+    private boolean isPAIR(List<Card> cardList) {
+    }
 }
