2ef65c40818128dfec5ecc9058f1652eb0edb426
swxss124 <swxss124@naver.com>
2017-06-15 16:37:42 +0900

    Create enum Zokbo : 족보 enum 생성

--- src/main/java/kr/ac/cnu/service/poker/Zokbo.java
+++ src/main/java/kr/ac/cnu/service/poker/Zokbo.java
@@ -1,7 +1,23 @@
 package kr.ac.cnu.service.poker;
 
 /**
- * Created by swxss on 2017-06-15.
+ * Created by YJM on 2017-06-15.
+ */
+
+/**
+ * https://www.pokerstars.com/poker/?no_redirect=1
+ * 위 사이트의 족보를 토대로 작성함.
+ * 위에서부터 아래로 내림차순
  */
 public enum Zokbo {
+    ROYAL_FLUSH(1),
+    STRAIGHT_FLUSH(2),
+    FOUR_OF_A_KIND(3),
+    FULL_HOUSE(4),
+    FLUSH(5),
+    STRAIGHT(6),
+    THREE_OF_A_KIND(7),
+    TWO_PAIR(8),
+    PAIR(9),
+    HIGH_CARD(10);
 }
