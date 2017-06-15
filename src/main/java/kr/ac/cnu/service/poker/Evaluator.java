06ec9f411c98c603004c0e451da2c5a500978303
BonCheul <bonchecul92nd@naver.com>
2017-06-15 17:15:23 +0900

    create isPAIR mehtod in Evalutor by boncheul

--- src/main/java/kr/ac/cnu/service/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/service/poker/Evaluator.java
@@ -1,7 +1,16 @@
 package kr.ac.cnu.service.poker;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by sizz on 2017-06-15.
  */
 public class Evaluator {
+
+    public boolean isPAIR(List<Card> cardList) {
+        Map <Integer, Integer> tempMap = new HashMap<Integer, Integer>();
+        return false;
+    }
 }
--- src/test/java/kr/ac/cnu/poker/DeckTest.java
+++ src/test/java/kr/ac/cnu/poker/DeckTest.java
@@ -4,4 +4,5 @@ package kr.ac.cnu.poker;
  * Created by dajab on 2017-06-15.
  */
 public class DeckTest {
+
 }
