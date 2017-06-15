68e57fe13e6decfd55f9d0a5d317f830f6b5dcd2
BonCheul <bonchecul92nd@naver.com>
2017-06-15 17:18:36 +0900

    fill inside of Evaluator-isPAIR method by boncheul

--- src/main/java/kr/ac/cnu/service/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/service/poker/Evaluator.java
@@ -11,6 +11,20 @@ public class Evaluator {
 
     public boolean isPAIR(List<Card> cardList) {
         Map <Integer, Integer> tempMap = new HashMap<Integer, Integer>();
+        for(Card card : cardList) {
+            if(tempMap.containsKey(card.getRank())) {
+                Integer count = tempMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                tempMap.put(card.getRank(), count);
+            } else {
+                tempMap.put(card.getRank(), new Integer(1));
+            }
+        }
+        for(Integer key : tempMap.keySet()) {
+            if(tempMap.get(key) == 2) {
+                return true;
+            }
+        }
         return false;
     }
 }
